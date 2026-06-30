2f1b4f95f04bef001c48a81dcedf690f09858205
jb-brahim <brahimojaballi@gmail.com>
2026-06-30 08:21:43 +0100

    feat: implement authentication routes for login, logout, and user profile updates

--- sosab-be/routes/auth.js
+++ sosab-be/routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { login, logout } = require('../controllers/authController');
+const { login, logout, updatePassword, updateDetails } = require('../controllers/authController');
 const { protect } = require('../middleware/auth');
 
 router.post('/login', login);
 router.post('/logout', protect, logout);
+router.put('/updatepassword', protect, updatePassword);
+router.put('/updatedetails', protect, updateDetails);
 
 module.exports = router;
 
